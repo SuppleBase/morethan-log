2103c55d8977f22149bfabee33e7992cd1e60942
SuppleBase <147074552+SuppleBase@users.noreply.github.com>
2023-10-19 16:44:13 +0200

    Update Logo.tsx

--- src/layouts/RootLayout/Header/Logo.tsx
+++ src/layouts/RootLayout/Header/Logo.tsx
@@ -5,11 +5,18 @@ import styled from "@emotion/styled"
 const Logo = () => {
   return (
     <StyledWrapper href="/" aria-label={CONFIG.blog.title}>
-      {CONFIG.blog.title}
+       <div className="name">{CONFIG.profile.name}</div>
     </StyledWrapper>
   )
 }
 
 export default Logo
 
-const StyledWrapper = styled(Link)``
+const StyledWrapper = styled(Link)`
+> .name {
+  font-size: 1.25rem;
+  line-height: 1.75rem;
+  font-style: italic;
+  font-weight: 700;
+}
+`
